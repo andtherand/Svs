c4b138824ec83e9553db1b362c57c66ca02c9fc6
Andreas Ruck <mychiara+github@gmail.com>
2011-11-18 22:37:55 +0100

    added the getter/Setter methods for the mapper

--- Model/Service/Abstract.php
+++ Model/Service/Abstract.php
@@ -1,17 +1,37 @@
 <?php
     
-class Svs_Model_Service_Abstract 
+abstract class Svs_Model_Service_Abstract 
 {
 	//-------------------------------------------------------------------------
 	// - VARS
 	
 	/**
-	 * @var Svs_Model_DataMapper_Abstract
+	 * @var 	Svs_Model_DataMapper_Abstract
 	 */
 	protected $_mapper; 
 		
 	//-------------------------------------------------------------------------
 	// - PUBLIC
+	
+	/**
+	 * @return 	Svs_Model_DataMapper_Abstract 
+	 */
+	public function getMapper()
+	{
+		return $this->_mapper;
+	}
+	
+	/**
+	 * sets the dataMapper for a service
+	 * provides a fluid interface
+	 * 
+	 * @param	Svs_Model_DataMapper_Abstract $mapper
+	 * @return 	Svs_Model_Service_Abstract 
+	 */
+	public function setMapper(Svs_Model_DataMapper_Abstract $mapper)
+	{
+		$this->_mapper = $mapper;
+	}
 	
 	//-------------------------------------------------------------------------
 	// - PROTECTED
