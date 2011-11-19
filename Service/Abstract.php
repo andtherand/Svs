e8f3d21f08b94727af399bab17c1d7e67468929d
Andreas Ruck <mychiara+github@gmail.com>
2011-11-19 21:42:28 +0100

    added the editableinterface to the service package

--- Service/Abstract.php
+++ Service/Abstract.php
@@ -6,14 +6,27 @@ abstract class Svs_Service_Abstract
 	// - VARS
 	
 	/**
+	 * handles the persistence
+	 *  
 	 * @var 	Svs_Model_DataMapper_Abstract
 	 */
 	protected $_mapper;
 	
 	/**
+	 * handles the validation and serves the viewable representation of 
+	 * the form to the controller
+	 * 
 	 * @var 	Zend_Form
 	 */
 	protected $_form; 
+	
+	/**
+	 * hands over the params to the service layer with the result of a really thin
+	 * contoller
+	 * 
+	 *  @var 	Zend_Controller_Request_Abstract
+	 */
+	protected $_request;
 		
 	//-------------------------------------------------------------------------
 	// - PUBLIC
@@ -37,58 +50,105 @@ abstract class Svs_Service_Abstract
 	public function getMapper($prefix = null, $type = null)
 	{
 		if(null === $this->_mapper && null !== $prefix && null !== $type){
-			$mapperType = sprintf('%s_Model_DataMapper_%s', $prefix, $type);
-			
-			if(!class_exists($mapperType)){ 
-				throw new Svs_Model_Exception(
-					sprintf(
-						'The given DataMapper %s does not exist',
-						$mapperType
-					)
-				);	
+			try {
+				$this->setMapper(sprintf(
+					'%s_Model_DataMapper_%s', $prefix, $type)
+				);
+				
+			} catch(Svs_Model_Exception $e){
+				throw $e;				
 			}
-			
-			$mapper = new $mapperType;
-			$this->setMapper($mapper);
 		}
 		
 		return $this->_mapper;
 	}
 	
 	/**
-	 * sets the dataMapper for a service
+	 * sets the dataMapper for a service 
 	 * provides a fluid interface
 	 * 
-	 * @param	Svs_Model_DataMapper_Abstract $mapper
+	 * @param	Svs_Model_DataMapper_Abstract|string $mapper a string or a mapper_abstract
+	 * @throws 	Svs_Model_Exception	when a given mapper class does not exist	
 	 * @return 	Svs_Model_Service_Abstract 
 	 */
-	public function setMapper(Svs_Model_DataMapper_Abstract $mapper)
+	public function setMapper($mapper)
 	{
-		$this->_mapper = $mapper;
-	}
-	
-	/**
-	 * sets the form for later validation of domain objects
-	 * provides a fluid interface
-	 * 
-	 * @param	Zend_Form	$form	an instance of zend form; for validation
-	 * @return 	Svs_Model_Service_Abstract
-	 */
-	public function setForm(Zend_Form $form)
-	{
-		$this->_form = $form;
+		if(is_string($mapper)){
+			if(!class_exists($mapper)){
+				throw new Svs_Model_Exception(
+					sprintf('The given mapper %s does not exist'), $mapper
+				);
+			}
+			$mapper = new $mapper();
+		}
+		
+		if($mapper instanceof Svs_Model_DataMapper_Abstract){
+			$this->_mapper = $mapper;
+		}
 		
 		return $this;
 	}
 	
 	/**
-	 * retrieves the given instance of the form
+	 * sets the form for later validation of domain objects.
+	 * takes a string or an instance of Zend_Form
+	 * provides a fluid interface
 	 * 
-	 * @return Zend_Form	
+	 * @param	string|Zend_Form $form an instance of zend form or a string; for validation
+	 * @throws	Svs_Model_Exception	when given the form does not exist 
+	 * @return 	Svs_Model_Service_Abstract
 	 */
-	public function getForm()
+	public function setForm($form)
 	{
-		return $this->_form;		
+		if(is_string($form)){
+			if(!class_exists($form)){
+				throw new Svs_Model_Exception(
+					sprintf('The given form %s does not exist'), $form
+				);
+			}
+			$form = new $form();
+		}
+		
+		if($form instanceof Zend_Form){
+			$this->_form = $form;
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * retrieves the given instance of the form in a lazyload manner
+	 * 
+	 * @param	[string $prefix e.g. the module name]
+	 * @param	[string $type e.g. the controller name]
+	 * @throws 	Svs_Model_Exception when the given form does not exist
+	 * @return 	Zend_Form	
+	 */
+	public function getForm($prefix = null, $type = null)
+	{
+		if(null === $this->_form && null !== $prefix && null !== $type){
+			try {
+				$this->setForm(sprintf('%s_Form_%s', $prefix, $type));
+				
+			} catch(Svs_Model_Exception $e){
+				throw $e;
+			}
+		}
+		
+		return $this->_form;
+	}
+	
+	/**
+	 * sets the current request object to handle logic in the service layer
+	 * provides a fluid interface
+	 * 
+	 * @param	Zend_Controller_Request_Abstract $r the current request object
+	 * @return	Svs_Service_Abstract
+	 */
+	public function setRequest(Zend_Controller_Request_Abstract $r)
+	{
+		$this->_request = $r;
+		return $this;
 	}
 	
 	//-------------------------------------------------------------------------
