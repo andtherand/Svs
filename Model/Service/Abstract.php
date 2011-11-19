21dc0cb69268e31c8e049f34a86a3be5e5838477
Andreas Ruck <mychiara+github@gmail.com>
2011-11-19 14:12:17 +0100

    added the ServiceLayerAbstractClass

--- Model/Service/Abstract.php
+++ Model/Service/Abstract.php
@@ -8,16 +8,35 @@ abstract class Svs_Model_Service_Abstract
 	/**
 	 * @var 	Svs_Model_DataMapper_Abstract
 	 */
-	protected $_mapper; 
+	protected $_mapper;
+	
+	/**
+	 * @var 	Zend_Form
+	 */
+	protected $_form; 
 		
 	//-------------------------------------------------------------------------
 	// - PUBLIC
 	
 	/**
+	 * retrieves a set mapper or lazyloads and sets a mapper
+	 * 
+	 * @param	[string $prefix  for example the module name]
+	 * @param	[string $type  for example the type name of the mapper]
+	 * @throws 	Svs_Model_Exception	when a given mapper class does not exist
 	 * @return 	Svs_Model_DataMapper_Abstract 
 	 */
-	public function getMapper()
+	public function getMapper($prefix = null, $type = null)
 	{
+		if(null === $this->_mapper && null !== $prefix && null !== $type){
+			$mapper = sprintf('%s_Model_DataMapper_%s', $prefix, $type);
+			if(class_exists($mapper)){ 
+				$this->setMapper($mapper);
+			}
+			throw new Svs_Model_Exception(
+				sprintf('The given DataMapper %s does not exist', $mapper)
+			);
+		}
 		return $this->_mapper;
 	}
 	
@@ -31,6 +50,30 @@ abstract class Svs_Model_Service_Abstract
 	public function setMapper(Svs_Model_DataMapper_Abstract $mapper)
 	{
 		$this->_mapper = $mapper;
+	}
+	
+	/**
+	 * sets the form for later validation of domain objects
+	 * provides a fluid interface
+	 * 
+	 * @param	Zend_Form	$form	an instance of zend form; for validation
+	 * @return 	Svs_Model_Service_Abstract
+	 */
+	public function setForm(Zend_Form $form)
+	{
+		$this->_form = $form;
+		
+		return $this;
+	}
+	
+	/**
+	 * retrieves the given instance of the form
+	 * 
+	 * @return Zend_Form	
+	 */
+	public function getForm()
+	{
+		return $this->_form;		
 	}
 	
 	//-------------------------------------------------------------------------
