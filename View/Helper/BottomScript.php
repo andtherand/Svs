de7d595c11fd4d7123adf68e37628fa129a9f026
Andy Ruck <mychiara@gmail.com>
2012-05-27 15:16:05 +0200

    added the bottomScriptHelper to the library
    -adds the baseUrl if needed to headScripts

---
+++ View/Helper/BottomScript.php
@@ -0,0 +1,66 @@
+<?php
+
+class Svs_View_Helper_BottomScript extends Zend_View_Helper_HeadScript
+{
+    //-------------------------------------------------------------------------
+    // - VARS
+
+    protected $_baseUrl = '/';
+
+    //-------------------------------------------------------------------------
+    // - METHODS
+
+    public function bottomScript($paths = null)
+    {
+        $this->_retrieveBaseUrl();
+
+        if (is_array($paths)) {
+            $this->appendFiles($paths);
+
+        } else if (null !== $paths) {
+            $this->appendFile($paths);
+        }
+
+        return $this;
+    }
+
+    private function _retrieveBaseUrl()
+    {
+       $baseUrl = Zend_Controller_Front::getInstance()->getBaseUrl();
+
+        if (null !== $baseUrl) {
+            $this->_baseUrl = $baseUrl . '/';
+        }
+    }
+
+    /**
+     * appends js files
+     *
+     * @param  array $files array of js file paths
+     * @return Zend_View_Helper_HeadScript
+     */
+    public function appendFiles(array $files)
+    {
+        foreach ($files as $file) {
+            $this->appendFile($file);
+        }
+    }
+
+    /**
+     * appends a file and adds the baseUrl
+     *
+     * @param  string $path
+     * @return Zend_View_Helper_HeadScript
+     */
+    public function appendFile($path)
+    {
+        $file = Svs_Utils_String::addBaseUrl($path, $this->_baseUrl);
+
+        parent::appendFile($file);
+        return $this;
+    }
+
+
+
+
+}
